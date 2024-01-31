1537d866f2759d3c545b3756af682a80ec94c0f2
Luca Patarca <luca.patarca@gmail.com>
2024-01-31 16:10:36 +0000

    Restarted with simulation

--- src/simulation/launch/sim.launch.py
+++ src/simulation/launch/sim.launch.py
@@ -1,35 +1,23 @@
-# Copyright 2020 Louise Poubel
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Launch Gazebo with a world that has Robot, as well as the follow node."""
+"""Launch Gazebo with a world that has Dolly, as well as the follow node."""
 
 import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
-from launch.conditions import IfCondition
+from launch.actions import (DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.substitutions import (
+    Command,
+    LaunchConfiguration,
+)
 
 
 def generate_launch_description():
 
     pkg_ros_ign_gazebo = get_package_share_directory('ros_ign_gazebo')
     pkg_simulation = get_package_share_directory('simulation')
+    gz_models_path = os.path.join(pkg_simulation, "models")
 
     # Gazebo launch
     gazebo = IncludeLaunchDescription(
@@ -37,58 +25,62 @@ def generate_launch_description():
             os.path.join(pkg_ros_ign_gazebo, 'launch', 'ign_gazebo.launch.py'),
         ),
     )
-
-    # TODO: expose pose as a launch argument
-    spawn = Node(package='ros_ign_gazebo', executable='create',
-                 arguments=[
-                    '-name', 'robot',
-                    '-x', '5.0',
-                    '-z', '0.46',
-                    '-Y', '1.57',
-                    '-file', os.path.join(pkg_simulation, 'models', 'robot_old',
-                                          'model.sdf')],
-                 output='screen')
     
-    # spawn = Node(package='ros_ign_gazebo', executable='create',
-    #              arguments=[
-    #                 '-name', 'dolly',
-    #                 '-x', '5.0',
-    #                 '-z', '0.46',
-    #                 '-Y', '1.57',
-    #                 '-file', os.path.join(pkg_simulation, 'models', 'dolly_ignition',
-    #                                       'model.sdf')],
-    #              output='screen')
-
-    # Bridge
-    bridge = Node(
-        package='ros_ign_bridge',
-        executable='parameter_bridge',
-        arguments=[
-            '/robot/laser_scan@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
-            '/robot/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist'
-            ],
-        output='screen'
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        namespace="pippo",
+        remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
+        parameters=[
+            {"robot_description": Command(["xacro ", LaunchConfiguration("model")])}
+        ],
     )
-
-    # RViz
-    rviz = Node(
-        package='rviz2',
-        executable='rviz2',
-        arguments=['-d', os.path.join(pkg_simulation, 'rviz', 'simulation.rviz')],
-        condition=IfCondition(LaunchConfiguration('rviz'))
+    
+    spawn = Node( 
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[ 
+            '-name', 'sam_bot',
+            '-topic', '/pippo/robot_description',
+            '-z', '0.2',
+            '-x', '-2',
+            '-y', '0',
+        ],
+        output='screen',
     )
+    
+    # spawn2 = Node( 
+    #     package='ros_gz_sim',
+    #     executable='create',
+    #     arguments=[ 
+    #         '-name', 'sam_bot2',
+    #         '-topic', 'robot_description',
+    #         '-z', '0.2',
+    #         '-x', '2',
+    #         '-y', '0',
+    #     ],
+    #     output='screen',
+    # )
 
     return LaunchDescription([
+        SetEnvironmentVariable(
+            name="IGN_GAZEBO_RESOURCE_PATH",
+            value=gz_models_path,
+        ),
+        SetEnvironmentVariable(
+            name="IGN_GAZEBO_MODEL_PATH",
+            value=gz_models_path,
+        ),
         DeclareLaunchArgument(
-          'ign_args',
-          default_value=[os.path.join(pkg_simulation, 'worlds', 'cave.world') +
-                         ' -v 2 --gui-config ' +
-                         os.path.join(pkg_simulation, 'ign', 'gui.config'), ''],
+          'gz_args',
+          default_value=[os.path.join(pkg_simulation, 'worlds', 'empty.sdf') + ' -v 2'],
           description='Ignition Gazebo arguments'),
-        DeclareLaunchArgument('rviz', default_value='true',
-                              description='Open RViz.'),
+        DeclareLaunchArgument(
+            name="model",
+            default_value=os.path.join(pkg_simulation, "urdf/sam_bot.urdf"),
+            description="Absolute path to robot urdf file",),
         gazebo,
+        robot_state_publisher,
         spawn,
-        bridge,
-        rviz
+        # spawn2,
     ])
