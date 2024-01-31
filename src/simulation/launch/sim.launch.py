3b1e898bb7ec4aa826a326b142768a9ae0a3264b
Luca Patarca <luca.patarca@gmail.com>
2024-01-31 21:57:20 +0000

    Single robot isolated and working

--- src/simulation/launch/sim.launch.py
+++ src/simulation/launch/sim.launch.py
@@ -3,14 +3,15 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import (DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable)
+from launch import Condition, LaunchDescription
+from launch.actions import (DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable, ExecuteProcess, RegisterEventHandler)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch.substitutions import (
     Command,
     LaunchConfiguration,
 )
+from launch.event_handlers import OnProcessExit
 
 
 def generate_launch_description():
@@ -30,7 +31,7 @@ def generate_launch_description():
         package="robot_state_publisher",
         executable="robot_state_publisher",
         namespace="pippo",
-        remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
+        # remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
         parameters=[
             {"robot_description": Command(["xacro ", LaunchConfiguration("model")])}
         ],
@@ -40,13 +41,64 @@ def generate_launch_description():
         package='ros_gz_sim',
         executable='create',
         arguments=[ 
-            '-name', 'sam_bot',
+            '-name', 'pippo',
             '-topic', '/pippo/robot_description',
             '-z', '0.2',
             '-x', '-2',
             '-y', '0',
         ],
         output='screen',
+    )
+    
+    load_joint_state_controller = ExecuteProcess(
+        name="activate_joint_state_broadcaster",
+        cmd=[
+            "ros2",
+            "control",
+            "load_controller",
+            "--set-state", "active",
+            "-c", "pippo/controller_manager",
+            "joint_state_broadcaster",
+        ],
+        shell=False,
+        output="screen",
+    )
+    
+    load_diff_drive_controller = ExecuteProcess(
+        name="activate_diff_drive_base_controller",
+        cmd=[
+            "ros2",
+            "control",
+            "load_controller",
+            "--set-state", "active",
+            "-c", "pippo/controller_manager",
+            "diff_drive_base_controller",
+        ],
+        shell=False,
+        output="screen",
+    )
+    
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        namespace='pippo',
+        name="rviz2",
+        # remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
+        output="screen",
+        arguments=["-d", os.path.join(pkg_simulation, "rviz/simulation.rviz")],
+    )
+    
+    bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/pippo/scan@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan",
+            "/pippo/imu@sensor_msgs/msg/Imu[ignition.msgs.IMU",
+            '/pippo/gps/fix@sensor_msgs/msg/NavSatFix[gz.msgs.NavSat',
+            # Clock message is necessary for the diff_drive_controller to accept commands https://github.com/ros-controls/gz_ros2_control/issues/106
+            "/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock",
+        ],
+        output="screen",
     )
     
     # spawn2 = Node( 
@@ -73,7 +125,7 @@ def generate_launch_description():
         ),
         DeclareLaunchArgument(
           'gz_args',
-          default_value=[os.path.join(pkg_simulation, 'worlds', 'empty.sdf') + ' -v 2'],
+          default_value=[os.path.join(pkg_simulation, 'worlds', 'empty.sdf') + ' -r'],
           description='Ignition Gazebo arguments'),
         DeclareLaunchArgument(
             name="model",
@@ -82,5 +134,19 @@ def generate_launch_description():
         gazebo,
         robot_state_publisher,
         spawn,
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn,
+                on_exit=[load_joint_state_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_diff_drive_controller],
+            )
+        ),
+        rviz_node,
+        bridge,
         # spawn2,
     ])
