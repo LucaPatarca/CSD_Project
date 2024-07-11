4518e342713aea2d86bc01aa4c66f987215cef3e
Luca Patarca <luca.patarca@gmail.com>
2024-07-11 18:02:02 +0200

    Add action topic & headless setup

--- src/tutorial_pkg/scripts/controller/controller.py
+++ src/tutorial_pkg/scripts/controller/controller.py
@@ -13,6 +13,7 @@ class Controller(Node):
     super().__init__('controller')
     self.charge_pub = self.create_publisher(String, 'battery_charge', 10)
     self.explore_pub = self.create_publisher(Bool, 'explore/resume', 10)
+    self.action_pub = self.create_publisher(Bool, 'action', 10)
     self.battery_sub = self.create_subscription(
             BatteryState,
             'battery_status',
@@ -21,9 +22,15 @@ class Controller(Node):
     self.battery_sub  # prevent unused variable warning
     self.waiting_to_charge = False
     self.nav = BasicNavigator()
+    
+  def publish_action(self, data: str):
+      action = String()
+      action.data = data
+      self.action_pub.publish(action)
 
   def battery_callback(self, msg: BatteryState):
     if(msg.percentage < 0.30 and msg._power_supply_status != BatteryState.POWER_SUPPLY_STATUS_CHARGING):
+      self.publish_action("Stop exploration")
       explore_msg = Bool()
       explore_msg.data = False
       self.explore_pub.publish(explore_msg)
@@ -33,12 +40,15 @@ class Controller(Node):
       goal_pose.header.stamp = self.nav.get_clock().now().to_msg()
       goal_pose.pose.position.x = 0.0
       goal_pose.pose.position.y = 0.0
+      self.publish_action("Return to base")
       self.nav.goToPose(goal_pose)
       while not self.nav.isTaskComplete():
         time.sleep(1)
+      self.publish_action("Start charging")
       self.charge_pub.publish(String())
       self.waiting_to_charge = True
     elif(self.waiting_to_charge and msg.power_supply_status != BatteryState.POWER_SUPPLY_STATUS_CHARGING):
+      self.publish_action("Restart exploration")
       explore_msg = Bool()
       explore_msg.data = True
       self.explore_pub.publish(explore_msg)
